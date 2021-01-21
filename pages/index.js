ef9817661a2ade7f56763fef78f704854cc5fb71
henry <ksn569@gmail.com>
2021-01-21 14:10:37 +0900

    hello world and add node modules

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+const index = () => {
+    return (
+        <Wrapper>
+        hello world      
+        </Wrapper>
+    );
+};
+
+export default index;
+const Wrapper = styled.div`
+    color: red;
+`;
